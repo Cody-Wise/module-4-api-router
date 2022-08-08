e0c2f15fc6b8bf22239531e015810ebb27b421cf
Cody-Wise <cody@sharptackmedia.com>
2022-08-08 16:58:19 -0700

    Commented out utils.js

--- __tests__/utils.js
+++ __tests__/utils.js
@@ -1,31 +1,31 @@
-const pool = require('../lib/utils/pool.js');
-const { readFileSync } = require('node:fs');
-const sql = readFileSync('./sql/setup.sql', 'utf-8');
-const request = require('supertest');
-const app = require('../lib/app');
+// const pool = require('../lib/utils/pool.js');
+// const { readFileSync } = require('node:fs');
+// const sql = readFileSync('./sql/setup.sql', 'utf-8');
+// const request = require('supertest');
+// const app = require('../lib/app');
 
-function setupDb() {
-  return pool.query(sql);
-}
+// function setupDb() {
+//   return pool.query(sql);
+// }
 
-function closeAll() {
-  return pool.end();
-}
+// function closeAll() {
+//   return pool.end();
+// }
 
-afterAll(closeAll);
+// afterAll(closeAll);
 
-const mockUser = {
-  email: 'test@example.com',
-  password: '123456',
-};
+// const mockUser = {
+//   email: 'test@example.com',
+//   password: '123456',
+// };
 
-async function signUpUser(credentials = mockUser) {
-  const agent = request.agent(app);
-  const res = await agent.post('/api/v1/user/signup').send(credentials);
-  return { agent, user: res.body, res, credentials };
-}
+// async function signUpUser(credentials = mockUser) {
+//   const agent = request.agent(app);
+//   const res = await agent.post('/api/v1/user/signup').send(credentials);
+//   return { agent, user: res.body, res, credentials };
+// }
 
-module.exports = {
-  setupDb,
-  signUpUser,
-};
+// module.exports = {
+//   setupDb,
+//   signUpUser,
+// };
